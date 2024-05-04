ba267172bd97867d04aeb43dd3f7bb6d22f7c4bc
vyomaa03 <vyomaa03@gmail.com>
2024-05-04 12:51:11 +1000

    Basic Navbar done

--- taskagotchi/src/components/NavigationBar/NavigationBar.jsx
+++ taskagotchi/src/components/NavigationBar/NavigationBar.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NavigationBar = () => {
   return (
-    <nav className="bg-[#FAF4E6]">
-      <h1>OMG NAVBAR</h1>
-    </nav>
+    <div className="w-full fixed top-0 left-0">
+      <div className="flex items-center justify-between bg-[#FAF4E6] py-4 md:px-10 px-7">
+        <div className="font-bold text-3xl cursor-pointer text-gray-800">
+          TaskaGotchi
+        </div>
+        <div className="text-2xl underline cursor-pointer text-gray-800">
+          About Us
+        </div>
+        <div className="text-2xl cursor-pointer text-gray-800">Login</div>
+      </div>
+    </div>
   );
 };
 
