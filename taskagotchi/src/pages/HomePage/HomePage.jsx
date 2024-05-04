e8127776fbb855ade7bbe27eddbdf245e0c5b829
vyomaa03 <vyomaa03@gmail.com>
2024-05-04 15:32:58 +1000

    div spacing test

--- taskagotchi/src/pages/HomePage/HomePage.jsx
+++ taskagotchi/src/pages/HomePage/HomePage.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 const HomePage = () => {
   return (
-    <div className="bg-[#FAF4E6]">
-      <h1>OMG HOME PAGFE</h1>
+    <div className="flex flex-col h-screen">
+      {/* Five equally sized divs */}
+      <div className="flex-grow flex flex-col justify-center items-center bg-[#FAF4E6]">
+        <div className="bg-red-500 flex-grow w-full">Div 1</div>
+        <div className="bg-blue-500 flex-grow w-full">Div 2</div>
+        <div className="bg-green-500 flex-grow w-full">Div 3</div>
+        <div className="bg-yellow-500 flex-grow w-full">Div 4</div>
+        <div className="bg-purple-500 flex-grow w-full">Div 5</div>
+      </div>
     </div>
   );
 };
