064cf5b0f158b34061bd9172c7a4a3e8d053aa44
Amy <amy2002phung@gmail.com>
2024-05-05 00:26:11 +1000

    updated foont

--- taskagotchi/tailwind.config.js
+++ taskagotchi/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
+      fontFamily: {
+        'sans': ['Montserrat', 'sans-serif']
+      },
       boxShadow: {
         pet: '12px 11px 0px 2px #45473F',
         custom: '8px 8px 0px -1px #45473F'
