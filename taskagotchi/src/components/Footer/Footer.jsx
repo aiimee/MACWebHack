4f8c2b9f14aaa561cb5ff50ff01b7bbf9bdeec36
vyomaa03 <vyomaa03@gmail.com>
2024-05-04 14:42:45 +1000

    Basic footer complete

--- taskagotchi/src/components/Footer/Footer.jsx
+++ taskagotchi/src/components/Footer/Footer.jsx
@@ -1,50 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Footer.css'
-
+import React from "react";
 function Footer() {
-    return (
-        <footer className='footer mt-auto py-3 bg-dark text-white'>
-            <div className='container text-center'>
-                <div className='row'>
-                    {/* About Section */}
-                    <div className='col-md-4'>
-                        <h5>About Soil</h5>
-                        <p>
-                            Nourishing communities with organic produce straight
-                            from our farms to your doorstep.
-                        </p>
-                    </div>
-
-                    {/* Navigation */}
-                    <div className='col-md-4'>
-                        <h5>Navigation</h5>
-                        <ul className='list-unstyled'>
-                            <li><Link to='/products'>Products</Link></li>
-                            <li><Link to='/specials'>Week's Specials</Link></li>
-                            <li><Link to='/diet-planner'>Diet Planner</Link></li>
-                        </ul>
-                    </div>
-
-                    {/* Social Media Links */}
-                    {/* The links dont go anywhere heh */}
-                    <div className='col-md-4'>
-                        <h5>Follow Us</h5>
-                        <ul className='list-unstyled'>
-                            <li><a href='#'>Facebook</a></li>
-                            <li><a href='#'>Instagram</a></li>
-                            <li><a href='#'>Twitter</a></li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div className='border-top pt-3'>
-                    <p>&copy; {new Date().getFullYear()} Soil. All rights reserved.</p>
-                </div>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="fixed bottom-0 left-0 w-full bg-black text-white">
+      <div className="flex flex-col justify-center items-center text-center pt-2 text-white text-sm">
+        <span className="mb-2 mt-2">Created by - Error 404</span>
+        <span className="mb-2 mt-2 pb-2">
+          © 2024 TaskaGotchi. All rights reserved.
+        </span>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
-
