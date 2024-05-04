55a0b2ccd4d7b4b90b5826768b7206db78d95119
vyomaa03 <vyomaa03@gmail.com>
2024-05-04 16:23:52 +1000

    First section completed

--- taskagotchi/src/pages/HomePage/HomePage.jsx
+++ taskagotchi/src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,37 @@
 import React from "react";
+import bookCharacter from "../../assets/images/BunnyBook.png";
 
 const HomePage = () => {
   return (
     <div className="flex flex-col h-screen">
-      {/* Five equally sized divs */}
-      <div className="flex-grow flex flex-col justify-center items-center bg-[#FAF4E6]">
-        <div className="bg-red-500 flex-grow w-full">Div 1</div>
-        <div className="bg-blue-500 flex-grow w-full">Div 2</div>
-        <div className="bg-green-500 flex-grow w-full">Div 3</div>
-        <div className="bg-yellow-500 flex-grow w-full">Div 4</div>
-        <div className="bg-purple-500 flex-grow w-full">Div 5</div>
+      <div className="flex-grow flex flex-col justify-center">
+        <div className="bg-[#FAF4E6] flex-grow w-full text-black flex justify-center">
+          <div className="max-w-screen-lg flex items-center">
+            <img
+              alt="character with book"
+              src={bookCharacter}
+              className="size-96 mr-10"
+            />
+            <div className="text-center w-70 ml-35">
+              <h1 className="text-2xl font-bold text-gray-800 mb-4 ml-">
+                Meet TaskaGotchi
+              </h1>
+              <p className="ml-8">
+                Where your productivity feeds your digital pet. Complete tasks,
+                level up, and bring joy to both your life and your virtual
+                buddy!
+              </p>
+              <button className="border-2 border-black text-black mt-5 py-2 px-4 rounded shadow-custom opacity-100">
+                Meet your TaskaGotchi Today
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-[#45473F] flex-grow w-full text-white">Div 2</div>
+        <div className="bg-[#FAF4E6] flex-grow w-full text-black">Div 3</div>
+        <div className="bg-[#45473F] flex-grow w-full text-white ">Div 4</div>
+        <div className="bg-[#FAF4E6] flex-grow w-full text-black">Div 5</div>
       </div>
     </div>
   );
