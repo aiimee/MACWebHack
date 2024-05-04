f999c7329f9d51fb4ac3503abce2d6ab5b5fddc4
Amy <amy2002phung@gmail.com>
2024-05-04 15:43:48 +1000

    moved footer to the bottom of the page

--- taskagotchi/src/components/Footer/Footer.jsx
+++ taskagotchi/src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 function Footer() {
   return (
-    <footer className="fixed bottom-0 left-0 w-full bg-black text-white">
+    <footer className="left-0 w-full bg-black text-white">
       <div className="flex flex-col justify-center items-center text-center pt-2 text-white text-sm">
         <span className="mb-2 mt-2">Created by - Error 404</span>
         <span className="mb-2 mt-2 pb-2">
