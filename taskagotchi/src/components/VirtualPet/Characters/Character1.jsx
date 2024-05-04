5432a1e65e83276799131bb49333fc52a77b7143
Amy <amy2002phung@gmail.com>
2024-05-05 05:10:38 +1000

    updated sprites and animation interval time

--- taskagotchi/src/components/VirtualPet/Characters/Character1.jsx
+++ taskagotchi/src/components/VirtualPet/Characters/Character1.jsx
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import SpriteAnimator from '../SpriteAnimator';
-import char1Idle from '../../../assets/images/character1/idle.png'
-import char1Idle2 from '../../../assets/images/character1/idle2.png'
-import char1Eating from '../../../assets/images/character1/eating.png'
-import char1Sleeping from '../../../assets/images/character1/sleeping.png'
-import char2Egg from '../../../assets/images/character2/egg.png'
-import char2Baby from '../../../assets/images/character2/baby.png'
-import char2Child from '../../../assets/images/character2/kid.png'
-import char2Adult from '../../../assets/images/character2/adult.png'
-
+import char1Idle from '../../../assets/images/character1/idle.png';
+import char1Idle2 from '../../../assets/images/character1/idle2.png';
+import char1Eating from '../../../assets/images/character1/eating.png';
+import char1Sleeping from '../../../assets/images/character1/sleeping.png';
+import char2Egg from '../../../assets/images/character2/egg.png';
+import char2Baby from '../../../assets/images/character2/baby.png';
+import char2Child from '../../../assets/images/character2/kid.png';
+import char2Adult from '../../../assets/images/character2/adult.png';
 
 const CharacterSheet = ({ character, experiencePoints, currentLevel, onExperienceChange, onLevelChange }) => {
   const [currentStage, setCurrentStage] = useState(() => {
@@ -18,20 +17,19 @@ const CharacterSheet = ({ character, experiencePoints, currentLevel, onExperienc
 
   // Dynamic assets based on character and stage
   const assets = {
-    // TEST character
     'character1': {
-      egg: { spriteSheet: char1Idle, frames: 2 },
-      baby: { spriteSheet: char1Idle2, frames: 2 },
-      kid: { spriteSheet: char1Eating, frames: 2 },
-      adult: { spriteSheet: char1Sleeping, frames: 2 }
+      egg: { spriteSheet: char1Idle, frames: 2, animationInterval: 1000 },
+      baby: { spriteSheet: char1Idle2, frames: 2, animationInterval: 900 },
+      kid: { spriteSheet: char1Eating, frames: 2, animationInterval: 800 },
+      adult: { spriteSheet: char1Sleeping, frames: 2, animationInterval: 700 }
     },
     'character2': {
-      egg: { spriteSheet: char2Egg, frames: 3 },
-      baby: { spriteSheet: char2Baby, frames: 16 },
-      kid: { spriteSheet: char2Child, frames: 16 },
-      adult: { spriteSheet: char2Adult, frames: 16 }
+      egg: { spriteSheet: char2Egg, frames: 16, animationInterval: 1400 },
+      baby: { spriteSheet: char2Baby, frames: 16, animationInterval: 800 },
+      kid: { spriteSheet: char2Child, frames: 16, animationInterval: 700 },
+      adult: { spriteSheet: char2Adult, frames: 16, animationInterval: 600 }
     },
-    // Add more sprite sheets characters here :3
+    // Add more characters and stages here
   };
 
   useEffect(() => {
@@ -59,20 +57,19 @@ const CharacterSheet = ({ character, experiencePoints, currentLevel, onExperienc
   const animationSettings = assets[character][currentStage];
 
   return (
-    <div>
-      <div className='character-container'>
-        <SpriteAnimator
-          spriteSheet={animationSettings.spriteSheet}
-          frameWidth={32}
-          frameHeight={16}
-          frameCount={animationSettings.frames}
-          animationInterval={500}
-        />
-      </div>
+    <div className='character-container'>
+      <SpriteAnimator
+        spriteSheet={animationSettings.spriteSheet}
+        frameWidth={32}
+        frameHeight={16}
+        frameCount={animationSettings.frames}
+        animationInterval={animationSettings.animationInterval}
+      />
     </div>
   );
 };
 
 export default CharacterSheet;
+
 
 
